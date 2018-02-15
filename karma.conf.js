30a42645cf1c4e8e23218374ab751ee1ad7fbd92
qizhang4 <qizhang4@illinois.edu>
2018-02-15 00:58:37 -0500

    Skip e2e test in CI process

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
